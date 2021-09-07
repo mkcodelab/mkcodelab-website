c09e01a51e0edbf7cf6e0d8c5eba89bf10657ce0
mikcodelab <michal.kulawik@gmail.com>
2021-09-07 17:07:14 +0200

    started repo

---
+++ script.js
@@ -0,0 +1,127 @@
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+let CW, CH;
+let rainQuantity = window.innerWidth;
+const circleArray = [];
+function resize() {
+  CW = canvas.width = rainQuantity = window.innerWidth;
+  CH = canvas.height = window.innerHeight
+  
+}
+resize();
+
+addEventListener('resize', ()=> {
+  resize();
+  populate(rainQuantity);
+});
+const mouse = {
+  x: null,
+  y: null,
+  size: 50,
+}
+// const mouseRadius = document.querySelector('#mouseRadius');
+// mouseRadius.style.width = mouse.size*2 + 'px';
+// mouseRadius.style.height = mouse.size*2 + 'px';
+
+addEventListener('mousemove', e => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  // mouseRadius.style.top = e.y + 'px';
+  // mouseRadius.style.left = e.x + 'px';
+  
+})
+
+class Circle {
+  constructor(x, y, col) {
+    this.x = x;
+    this.y = y;
+    this.baseSize = Math.random() * 2 + 1;
+    this.size = this.baseSize;
+    this.angle = 0;
+    this.baseSpeedY = (Math.random() * 5+1) /2;
+    this.speedY = this.baseSpeedY;
+    this.speedX = 0;
+    this.col = col;
+    this.frame = 0;
+    this.maxStep = 20;
+    this.step = 0;
+    this.shadow = false;
+  }
+  update() {
+    
+    if (this.y > canvas.height) {
+      this.y = 0 - this.size
+      this.speedY = this.baseSpeedY;
+    };
+    if (this.x > canvas.width) this.x = 0 - this.size;
+    this.y += this.speedY;
+    this.angle += 1
+    this.frame += Math.floor(Math.random()* 5);
+    if (this.frame % 5 == 0){
+      this.x += this.speedX;
+      this.speedX = Math.random() * 2 * Math.cos(this.angle/10);
+    }
+    
+    this.speedY += 0.02;
+
+  }
+  draw() {
+    if (this.shadow) {
+      ctx.shadowColor = 'white';
+      ctx.shadowBlur = 105;
+    }
+    ctx.fillStyle = this.col;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.shadowBlur = 0;
+  }
+  isColliding() {
+    let dx = this.x - mouse.x;
+    let dy = this.y - mouse.y;
+    let dist = Math.sqrt(dx*dx + dy*dy);
+    let sumOfRadii = this.size + mouse.size;
+
+    if (dist < sumOfRadii) {
+      let sign = Math.random() > .5 ? 1 : -1;
+      this.x += 2 * sign;
+      this.speedY = this.baseSpeedY;
+      this.y -= this.speedY;
+      
+      // this.size += 0.01;
+      this.shadow = true;
+    } else {
+      this.size = this.baseSize;
+      this.shadow = false;
+    }
+  }
+}
+
+
+
+function populate(q) {
+  circleArray.length = 0;
+  for (let i = 0; i < q; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    
+    let hue = Math.random() * 50 + 120;
+    let col = `hsla(${hue}, 100%, 50%, 0.2)`
+    circleArray.push( new Circle(x, y, col))
+  }
+}
+populate(rainQuantity);
+
+
+function loop() {
+  ctx.fillStyle = 'rgba(0,0,0,.1)';
+  ctx.fillRect(0, 0, CW, CH);
+  for (circ of circleArray) {
+    circ.update();
+    circ.draw();
+    circ.isColliding();
+  }
+  requestAnimationFrame(loop)
+}
+loop();
---
+++ slider.js
@@ -0,0 +1,67 @@
+const projectSlider = document.querySelector('#project-slider');
+const navLeft = document.querySelector('#projLeft');
+const navRight = document.querySelector('#projRight');
+
+const projects = [
+  {
+    name: 'Space Defender',
+    descr: 'Gra typu planet defense, osadzona w przestrzeni kosmicznej',
+    link: 'https://mkcodelab.github.io/Space-Defender/',
+    img: 'img/spacedefender.png',
+    alt: 'spaceDefender.png'
+  },
+  {
+    name: 'Backmasking',
+    descr: 'Apka, dzięki której możesz wgrać swój plik muzyczny i odtworzyć go wspak',
+    link: 'https://mkcodelab.github.io/Backmasking/',
+    img: 'img/backmasking.png',
+    alt: 'backmasking.png'
+  },
+  {
+    name: 'Vue.js Color Picker',
+    descr: 'Color picker napisany w vue, rgba, hsla, hex',
+    link: 'https://mkcodelab.github.io/vue.js-color-picker/',
+    img: 'img/colorpicker.png',
+    alt: 'colorpicker.png'
+  },
+  {
+    name: 'Background Generator',
+    descr: 'Apka do tworzenia tła ekranu.',
+    link: 'https://mkcodelab.github.io/Background-Generator/',
+    img: 'img/backgroundgen.png',
+    alt: 'backgroundgen.png'
+
+  }
+]
+
+let current = 0;
+projectSlider.innerHTML = `
+<h1 class="project-title">${projects[current].name}</h1>
+<a class="project-link" href="${projects[current].link}">
+  <img class="project-img" src="${projects[current].img}" alt="bgMaker.jpg">
+</a>
+<p class="project-descr">${projects[current].descr}</p>
+`
+
+function moveTo(project) {
+  projectSlider.innerHTML = `
+  <h1 class="project-title">${projects[project].name}</h1>
+  <a class="project-link" href="${projects[project].link}">
+    <img class="project-img" src="${projects[project].img}" alt="bgMaker.jpg">
+  </a>
+  <p class="project-descr">${projects[project].descr}</p>
+  `
+}
+navRight.addEventListener('click', ()=> {
+  if (current >= projects.length-1) current = 0;
+  else current ++;
+  // console.log('current project is', current)
+  moveTo(current)
+});
+
+navLeft.addEventListener('click', ()=>{
+  if (current == 0) current = projects.length-1;
+  else current --;
+  // console.log('current project is', current)
+  moveTo(current)
+})
